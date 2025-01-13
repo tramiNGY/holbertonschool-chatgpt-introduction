4f293baa9812fb92678d52d83c65ff6a795db106
tramiNGY <ngytrami@gmail.com>
2025-01-13 13:56:06 +0100

    Modified again

--- debugging/mines.py
+++ debugging/mines.py
@@ -9,7 +9,13 @@ class Minesweeper:
     def __init__(self, width=10, height=10, mines=10):
         self.width = width
         self.height = height
-        self.mines = set(random.sample(range(width * height), mines))
+        # Create a set of (x, y) coordinates for mines
+        self.mines = set()
+        while len(self.mines) < mines:
+            x = random.randint(0, width - 1)
+            y = random.randint(0, height - 1)
+            self.mines.add((x, y))
+        
         self.field = [[' ' for _ in range(width)] for _ in range(height)]
         self.revealed = [[False for _ in range(width)] for _ in range(height)]
 
@@ -20,7 +26,7 @@ class Minesweeper:
             print(y, end=' ')
             for x in range(self.width):
                 if reveal or self.revealed[y][x]:
-                    if (y * self.width + x) in self.mines:
+                    if (x, y) in self.mines:
                         print('*', end=' ')
                     else:
                         count = self.count_mines_nearby(x, y)
@@ -35,12 +41,12 @@ class Minesweeper:
             for dy in [-1, 0, 1]:
                 nx, ny = x + dx, y + dy
                 if 0 <= nx < self.width and 0 <= ny < self.height:
-                    if (ny * self.width + nx) in self.mines:
+                    if (nx, ny) in self.mines:
                         count += 1
         return count
 
     def reveal(self, x, y):
-        if (y * self.width + x) in self.mines:
+        if (x, y) in self.mines:
             return False
         self.revealed[y][x] = True
         if self.count_mines_nearby(x, y) == 0:
